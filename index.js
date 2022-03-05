9726b0ec508c4828df2bf03a1d379f2908490652
itstime4masti <itstime4masti@gmail.com>
2022-03-05 17:55:28 +0530

    inting

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 5800
 var excelData = [];
 var cors = require ('cors');
 app.use(cors({
-    origin:['https://exceltotally.netlify.app','https://exceltotally.netlify.app//free.html','https://exceltotally.herokuapp.com/upload'],
+    origin:['https://exceltotally.netlify.app','https://exceltotally.netlify.app/free.html','https://exceltotally.herokuapp.com/upload'],
     credentials:true
 }));
 
@@ -40,7 +40,7 @@ app.use('/upload',(req, res, next) =>  {
             console.log(JSON.parse(jsonString));
 			excelData = JSON.parse(jsonString);
 			voucher();
-			res.header("Access-Control-Allow-Origin", "https://exceltotally.netlify.app/");
+			res.header("Access-Control-Allow-Origin", "https://exceltotally.netlify.app","https://exceltotally.netlify.app/free.html","https://exceltotally.herokuapp.com/upload");
 			res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept";
 			res.header('Access-Control-Allow-Credentials', 'Content-Type');
 			res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
